98201d45ff3ec28b086c0c3d831c0b87934699e0
Moeka Kiryu <moekakiryutaku@gmail.com>
2023-08-06 21:41:01 +1000

    Create basic View component functionality

---
+++ src/components/View/SizeControl/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import cx from "classnames";
+
+import styles from './styles.scss'
+
+export const POSITION = {
+  top: 'top',
+  bottom: 'bottom',
+  left: 'left',
+  right: 'right',
+}
+
+function SizeControl({ isVisible, position }) {
+  // Note: Below we could create a mapping from POSITION values to their
+  //       matching styles. While that would be more concise, this has the
+  //       benefit of being readable at a glance (unlike this comment)
+  const sizeControl = (
+    <span
+      className={cx(
+        styles.sizeControl,
+        {
+          [styles.top]:    position === POSITION.top,
+          [styles.bottom]: position === POSITION.bottom,
+          [styles.left]:   position === POSITION.left,
+          [styles.right]:  position === POSITION.right,
+        }
+      )}
+    />
+  )
+
+  return (
+    <React.Fragment>
+      {isVisible && sizeControl}
+    </React.Fragment>
+  )
+}
+
+export default SizeControl
--- src/components/View/index.jsx
+++ src/components/View/index.jsx
@@ -1,11 +1,65 @@
-import React from "react";
+import React, { useState, useRef } from "react"
+import _pickBy from 'lodash/pickBy'
+import _keys from 'lodash/keys'
+import _head from 'lodash/head'
+
+import SizeControl, { POSITION } from "./SizeControl"
 
 import styles from './styles.scss'
 
+const getHoverRegions = (container, mouseX, mouseY, options = { regionPercent: 0.05 }) => {
+    const {
+        offsetTop: containerTop,
+        offsetLeft: containerLeft,
+        offsetWidth: containerWidth,
+        offsetHeight: containerHeight
+    } = container
+
+    const offsetX = mouseX - containerLeft;
+    const offsetY = mouseY - containerTop;
+
+    const regionWidth = containerWidth * options.regionPercent;
+    const regionHeight = containerHeight * options.regionPercent;
+
+    const activeRegions = {
+        [POSITION.left]:   offsetX < regionWidth,
+        [POSITION.top]:    offsetY < regionHeight,
+        [POSITION.bottom]: (containerHeight - offsetY) < regionHeight,
+        [POSITION.right]:  (containerWidth - offsetX) < regionWidth,
+    }
+
+    return activeRegions
+}
+
 function View() {
+    const [activeRegion, setActiveRegion] = useState(null)
+    const containerRef = useRef()
+
+    const onMouseMove = evt => {
+        const regions = getHoverRegions(containerRef.current, evt.clientX, evt.clientY)
+
+        const activeRegions = _pickBy(regions, (v,k) => v)
+        const firstActive = _head(_keys(activeRegions))
+
+        setActiveRegion(firstActive || null)
+
+    }
+
+    const onMouseLeave = () => setActiveRegion(null)
+
     return (
-        <div class={styles.container}>
-            Hello, world!
+        <div
+            ref={containerRef}
+            className={styles.view}
+            onMouseMove={onMouseMove}
+            onMouseLeave={onMouseLeave}
+        >
+            <SizeControl
+                isVisible={!!activeRegion}
+                position={activeRegion}
+            />
+            <div className={styles.content}>
+            </div>
         </div>
     )
 }
