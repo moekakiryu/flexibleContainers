e7ec116c8b22a7b09250c0273ded04e607be76f1
Moeka Kiryu <moekakiryutaku@gmail.com>
2023-08-06 21:37:40 +1000

    Replace boilerplate app with controller placeholder

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-import View from 'components/View'
+import ViewController from "components/ViewController";
 
 function App() {
   return (
-    <div className="App">
-      <View />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ViewController />
   );
 }
 
---
+++ src/components/ViewController/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import View from 'components/View'
+
+import styles from './styles.scss'
+
+function ViewController() {
+    return (
+        <div className={styles.controller}>
+            <View />
+        </div>
+    )
+}
+
+export default ViewController
