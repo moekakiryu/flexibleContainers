75c62b20bb2022d43b02a58018bcf612ac17811d
Moeka Kiryu <moekakiryutaku@gmail.com>
2023-08-14 15:28:39 +1000

    Change file indentation to two spaces

--- src/components/ViewController/View/index.jsx
+++ src/components/ViewController/View/index.jsx
@@ -7,90 +7,90 @@ import SizeControl, { POSITION } from "../SizeControl"
 import styles from './styles.scss'
 
 function View({
-    width,
-    height,
-    isDragged,
-    requestResize,
-    ...otherProps
+  width,
+  height,
+  isDragged,
+  requestResize,
+  ...otherProps
 }) {
-    const containerRef = useRef()
-    const [ activeControl, setActiveControl ] = useState(null)
+  const containerRef = useRef()
+  const [ activeControl, setActiveControl ] = useState(null)
 
-    const getControlRegions = (container, mouseX, mouseY, options = { regionSize: 0.1 }) => {
-        const {
-            offsetTop: containerTop,
-            offsetLeft: containerLeft,
-            offsetWidth: containerWidth,
-            offsetHeight: containerHeight
-        } = container
+  const getControlRegions = (container, mouseX, mouseY, options = { regionSize: 0.1 }) => {
+    const {
+      offsetTop: containerTop,
+      offsetLeft: containerLeft,
+      offsetWidth: containerWidth,
+      offsetHeight: containerHeight
+    } = container
 
-        const offsetX = mouseX - containerLeft;
-        const offsetY = mouseY - containerTop;
+    const offsetX = mouseX - containerLeft;
+    const offsetY = mouseY - containerTop;
 
-        const regionWidth = containerWidth * options.regionSize;
-        const regionHeight = containerHeight * options.regionSize;
+    const regionWidth = containerWidth * options.regionSize;
+    const regionHeight = containerHeight * options.regionSize;
 
-        const activeRegions = {
-            [POSITION.left]:   offsetX < regionWidth,
-            [POSITION.top]:    offsetY < regionHeight,
-            [POSITION.bottom]: (containerHeight - offsetY) < regionHeight,
-            [POSITION.right]:  (containerWidth - offsetX) < regionWidth,
+    const activeRegions = {
+      [POSITION.left]:   offsetX < regionWidth,
+      [POSITION.top]:    offsetY < regionHeight,
+      [POSITION.bottom]: (containerHeight - offsetY) < regionHeight,
+      [POSITION.right]:  (containerWidth - offsetX) < regionWidth,
+    }
+
+    return activeRegions
+  }
+
+  const filterControlRegions = regions => {
+    const activeRegions = _pickBy(regions, (isActive, position) => isActive)
+
+    return _keys(activeRegions)[0]
+  }
+
+  const onMouseMove = ({ clientX, clientY }) => {
+    const controlRegions = getControlRegions(containerRef.current, clientX, clientY)
+    const newActiveControl = filterControlRegions(controlRegions) || null
+
+    // IMPORTANT: Page components can NOT be children of this component or else they will ALWAYS re-render on mouse move
+    //            Use an alternate method to render them or use shouldComponentUpdate
+    if (!isDragged && activeControl !== newActiveControl) {
+      setActiveControl(newActiveControl)
+    }
+  }
+
+  const onMouseDown = ({ clientX, clientY }) => {
+    if (activeControl) {
+      requestResize({
+        id: '<view uuid>',
+        direction: activeControl,
+        origin: {
+          x: clientX,
+          y: clientY,
         }
-
-        return activeRegions
+      })
     }
+  }
 
-    const filterControlRegions = regions => {
-        const activeRegions = _pickBy(regions, (isActive, position) => isActive)
-
-        return _keys(activeRegions)[0]
+  const onMouseLeave = () => {
+    if (!isDragged) {
+      setActiveControl(null)
     }
+  }
 
-    const onMouseMove = ({ clientX, clientY }) => {
-        const controlRegions = getControlRegions(containerRef.current, clientX, clientY)
-        const newActiveControl = filterControlRegions(controlRegions) || null
-
-        // IMPORTANT: Page components can NOT be children of this component or else they will ALWAYS re-render on mouse move
-        //            Use an alternate method to render them or use shouldComponentUpdate
-        if (!isDragged && activeControl !== newActiveControl) {
-            setActiveControl(newActiveControl)
-        }
-    }
-
-    const onMouseDown = ({ clientX, clientY }) => {
-        if (activeControl) {
-            requestResize({
-                id: '<view uuid>',
-                direction: activeControl,
-                origin: {
-                    x: clientX,
-                    y: clientY,
-                }
-            })
-        }
-    }
-
-    const onMouseLeave = () => {
-        if (!isDragged) {
-            setActiveControl(null)
-        }
-    }
-
-    // TODO: Otherprops should be passed to the viewed component
-    return (
-        <div
-            ref={containerRef}
-            className={styles.view}
-            style={{ width, height }}
-            onMouseMove={onMouseMove}
-            onMouseDown={onMouseDown}
-            onMouseLeave={onMouseLeave}
-            {...otherProps}
-        >
-            <SizeControl position={activeControl} />
-            <div className={styles.content} />
-        </div>
-    )
+  // TODO: Otherprops should be passed to the viewed component
+  return (
+    <div
+      ref={containerRef}
+      className={styles.view}
+      style={{ width, height }}
+      onMouseMove={onMouseMove}
+      onMouseDown={onMouseDown}
+      onMouseLeave={onMouseLeave}
+      {...otherProps}
+    >
+      <SizeControl position={activeControl} />
+      <div className={styles.content} />
+    </div>
+  )
 }
 
 export default View;
--- src/components/ViewController/index.jsx
+++ src/components/ViewController/index.jsx
@@ -6,56 +6,56 @@ import { POSITION } from "./SizeControl";
 import styles from './styles.scss'
 
 function ViewController() {
-    const [ elementWidth, setElementWidth ] = useState(400) // TODO: make the default better
-    const [ elementHeight, setElementHeight ] = useState(400) // TODO: make the default better
+  const [ elementWidth, setElementWidth ] = useState(400) // TODO: make the default better
+  const [ elementHeight, setElementHeight ] = useState(400) // TODO: make the default better
 
-    const [ dragOrigin, setDragOrigin ] = useState(null)
-    const [ resizeDirection, setResizeDirection ] = useState(null)
+  const [ dragOrigin, setDragOrigin ] = useState(null)
+  const [ resizeDirection, setResizeDirection ] = useState(null)
 
-    const requestResize = ({ id, direction, origin }) => {
-        // TODO: Update element based on ID
-        setDragOrigin(origin)
-        setResizeDirection(direction)
+  const requestResize = ({ id, direction, origin }) => {
+    // TODO: Update element based on ID
+    setDragOrigin(origin)
+    setResizeDirection(direction)
+  }
+
+  const resizeActiveView = ({ clientX, clientY }) => {
+    if (!dragOrigin) {
+      return
+    }
+    const isHorizontal = resizeDirection === POSITION.left || resizeDirection === POSITION.right
+    const isVertical = resizeDirection === POSITION.top || resizeDirection === POSITION.bottom
+
+    if (isHorizontal) {
+      setElementWidth(prevWidth => prevWidth + (clientX - dragOrigin.x))
+    } else if (isVertical) {
+      setElementHeight(prevHeight => prevHeight + (clientY - dragOrigin.y))
+    } else {
+      console.error(`Can't resize in unknown direction: '${resizeDirection}'`)
     }
 
-    const resizeActiveView = ({ clientX, clientY }) => {
-        if (!dragOrigin) {
-            return
-        }
-        const isHorizontal = resizeDirection === POSITION.left || resizeDirection === POSITION.right
-        const isVertical = resizeDirection === POSITION.top || resizeDirection === POSITION.bottom
+    setDragOrigin({
+      x: clientX,
+      y: clientY,
+    })
+  }
 
-        if (isHorizontal) {
-            setElementWidth(prevWidth => prevWidth + (clientX - dragOrigin.x))
-        } else if (isVertical) {
-            setElementHeight(prevHeight => prevHeight + (clientY - dragOrigin.y))
-        } else {
-            console.error(`Can't resize in unknown direction: '${resizeDirection}'`)
-        }
+  const clearDragOrigin = () => setDragOrigin(null)
 
-        setDragOrigin({
-            x: clientX,
-            y: clientY,
-        })
-    }
-
-    const clearDragOrigin = () => setDragOrigin(null)
-
-    return (
-        <div
-            className={styles.controller}
-            onMouseMove={resizeActiveView}
-            onMouseUp={clearDragOrigin}
-            onMouseLeave={clearDragOrigin}
-        >
-                <View
-                    width={elementWidth}
-                    height={elementHeight}
-                    isDragged={!!dragOrigin}
-                    requestResize={requestResize}
-                />
-        </div>
-    )
+  return (
+    <div
+      className={styles.controller}
+      onMouseMove={resizeActiveView}
+      onMouseUp={clearDragOrigin}
+      onMouseLeave={clearDragOrigin}
+    >
+        <View
+          width={elementWidth}
+          height={elementHeight}
+          isDragged={!!dragOrigin}
+          requestResize={requestResize}
+        />
+    </div>
+  )
 }
 
 export default ViewController
