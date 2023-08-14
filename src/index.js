45eb8ef3e6c8269a0cbf3bc60844ac8d974f84ff
Moeka Kiryu <moekakiryutaku@gmail.com>
2023-08-14 14:22:18 +1000

    (Unstable) Move CSS into 'shared' folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
-import 'styles/main.scss'
+import 'shared/styles/main.scss'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
