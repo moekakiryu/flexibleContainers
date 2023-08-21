2cfe3f889bc5c57df7e60dcfb062e2de991a1bdb
Moeka Kiryu <moekakiryutaku@gmail.com>
2023-08-21 22:14:38 +1000

    Make default layout more complex for testing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ViewController from "components/ViewController";
 const defaultLayout = {
   children: [
     {
-      id: 'c1',
+      id: 'v1',
       width: ( 1 / 4 ),
       height: 1,
     },
@@ -13,17 +13,17 @@ const defaultLayout = {
       height: 1,
       children: [
         {
-          id: 'c2r1',
+          id: 'c2v1',
           width: 1,
-          height: ( 1 / 4 ),
+          height: ( 1 / 3 ),
         },
         {
-          id: 'c2r2',
+          id: 'c2v2',
           width: 1,
-          height: ( 1 / 2 )
+          height: ( 5 / 12 )
         },
         {
-          id: 'c2r3',
+          id: 'c2v3',
           width: 1,
           height: ( 1 / 4 )
         }
@@ -33,30 +33,30 @@ const defaultLayout = {
       id: 'c3',
       width: ( 1 / 4 ),
       height: 1,
-      // children: [
-      //   {
-      //     id: 'c3r1',
-      //     width: 1,
-      //     height: ( 1 / 4 ),
-      //     children: [
-      //       {
-      //         id: 'c3r1c1',
-      //         width: ( 1 / 2 ),
-      //         height: 1
-      //       },
-      //       {
-      //         id: 'c3r1c2',
-      //         width: ( 1 / 2 ),
-      //         height: 1
-      //       },
-      //     ]
-      //   },
-      //   {
-      //     id: 'c3r2',
-      //     width: 1,
-      //     height: ( 3 / 4 ),
-      //   }
-      // ]
+      children: [
+        {
+          id: 'c3r1',
+          width: 1,
+          height: ( 1 / 4 ),
+          children: [
+            {
+              id: 'c3r1v1',
+              width: ( 1 / 2 ),
+              height: 1
+            },
+            {
+              id: 'c3r1v2',
+              width: ( 1 / 2 ),
+              height: 1
+            },
+          ]
+        },
+        {
+          id: 'c3v2',
+          width: 1,
+          height: ( 3 / 4 ),
+        }
+      ]
     }
   ]
 }
