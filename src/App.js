e95ed32184d5f6301ffd7b9fc06b95ffb76c0799
Moeka Kiryu <moekakiryutaku@gmail.com>
2023-09-02 17:27:42 +1000

    Change temporary layout id format again for better clarity

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ViewController from 'components/ViewController'
 const defaultLayout = {
   children: [
     {
-      id: 'v1',
+      id: 'c1/v',
       width: ( 1 / 4 ),
       height: 1,
     },
@@ -13,17 +13,17 @@ const defaultLayout = {
       height: 1,
       children: [
         {
-          id: 'c2v1',
+          id: 'c2r1/v',
           width: 1,
           height: ( 1 / 3 ),
         },
         {
-          id: 'c2v2',
+          id: 'c2r2/v',
           width: 1,
           height: ( 5 / 12 )
         },
         {
-          id: 'c2v3',
+          id: 'c2r3/v',
           width: 1,
           height: ( 1 / 4 )
         }
@@ -40,19 +40,19 @@ const defaultLayout = {
           height: ( 1 / 4 ),
           children: [
             {
-              id: 'c3r1v1',
+              id: 'c3r1c1/v',
               width: ( 1 / 2 ),
               height: 1
             },
             {
-              id: 'c3r1v2',
+              id: 'c3r1c2/v',
               width: ( 1 / 2 ),
               height: 1
             },
           ]
         },
         {
-          id: 'c3v2',
+          id: 'c3r2/v',
           width: 1,
           height: ( 3 / 4 ),
         }
@@ -97,7 +97,7 @@ function App() {
   return (
     <ViewController
       controllerId="root"
-      layout={debugLayout}
+      layout={defaultLayout}
     />
   );
 }
