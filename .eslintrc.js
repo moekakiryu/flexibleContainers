c5dddcca42a49a555db1b19bf4201936b6269d0d
Moeka Kiryu <moekakiryutaku@gmail.com>
2023-09-05 18:36:07 +1000

    Add eslint based on airbnb styleguide

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    semi: ['error', 'never'],
+  },
+  ignorePatterns: [
+    'node_modules/',
+    'scripts/',
+    'public/',
+    'config/',
+  ],
+}
